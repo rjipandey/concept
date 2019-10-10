9b0f5a49fd1638a1160c45e5992f9ff797e98f20
rjipandey <raj.xyz6@gmail.com>
2019-10-10 23:40:19 +0530

    added time count.

--- fibon/views.py
+++ fibon/views.py
@@ -1,12 +1,19 @@
+import time
 from django.shortcuts import render
 from functools import reduce
+from datetime import datetime, timedelta
 
-# Create your views here.
+
+
+# do your work here
+
+
 
 def index(request):
     return render(request, 'index.html')
 
 def fibonacci(request):
+    start_time = datetime.now()
     N=int(request.GET.get('N'))
     if N==0:
         result=0
@@ -15,5 +22,8 @@ def fibonacci(request):
     else:
         fib = lambda n: reduce(lambda x, _: x+[x[-1]+x[-2]], range(n-1), [0, 1])
         result=fib(N)[-1]
-    print(N)
-    return render(request, 'fib.html', {'result':result, 'N':N})
+    end_time = datetime.now()
+    total=end_time-start_time
+    context={'result':result, 'N':N, 'total':total*1000}
+    return render(request, 'fib.html', context)
+
